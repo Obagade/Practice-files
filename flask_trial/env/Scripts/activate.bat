12e775899e7a7a36ab0e25e72c8b715a8324da25
Obagade <rotimiobags@gmail.com>
2020-04-29 21:27:05 +0200

    modified virtual env

--- flask_trial/env/Scripts/activate.bat
+++ flask_trial/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\rotim\desktop\pythonpractice\flask_trial\env"
+set "VIRTUAL_ENV=C:\Users\rotim\Desktop\PythonPractice\flask_trial\env"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"

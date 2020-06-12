5ef6c80846957cea8f31c1b0cb896c16946682c9
Obagade <rotimiobags@gmail.com>
2020-06-13 01:10:23 +0200

    template literals

---
+++ ES6/templateLiteral.js
@@ -0,0 +1,11 @@
+let name = 'Timi';
+let age = 23;
+
+console.log(`My name is ${name} and my age is ${age}`);
+
+//To put things on a separate line
+
+console.log(`hey man
+what is 
+ going on
+ with ${name}`);
